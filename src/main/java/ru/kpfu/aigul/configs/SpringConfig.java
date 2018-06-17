59053629fa4e87f5f964eb60de4ac9c8c0cf799f
Aigulka <aigul050498@gmail.com>
2018-06-17 17:28:29 +0300

    remove

--- src/main/java/ru/kpfu/aigul/configs/SpringConfig.java
+++
@@ -1,8 +0,0 @@
-package ru.kpfu.aigul.configs;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("ru.kpfu.aigul")
-public class SpringConfig {}
